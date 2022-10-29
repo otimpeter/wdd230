7e3c63e98cda47f708061467c61aa1b3fa5a5c6d
Peter Charles Otim <113760237+otimpeter@users.noreply.github.com>
2022-10-29 20:33:20 +0300

    Add files via upload
    new changes on discover page

--- lesson7/scripts/script.js
+++ lesson7/scripts/script.js
@@ -1,3 +1,29 @@
+<<<<<<< HEAD
+let daynames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday"];
+let months = ["January", "Febuary", "March", "April", "May", "June",
+"July", "August", "September", "October", "Novermber", "December"];
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let fulldate = dayName + "," + monthName + " " + d.getDate() + "," + d.getFullYear();
+
+document.getElementById("currentdate"), textContent = fulldate;
+
+try {
+    let options = {
+        weekday: "long", 
+        day: "muemeric",
+        month: "long",
+        year: "numeric"
+    }
+    document.getElementById(
+        "currentdate2"
+        ).textContent = new Date().toLocaleDateString("en-US", Option);
+     } catch (e) {
+        alert("error with code or your broweswer does not support locale");
+     };
+=======
 
 const imagesToload = document.querySelectorAll("img[data-src]");
 
@@ -27,3 +53,4 @@ else {// images not supported
         loadImages(img);
     });
 }
+>>>>>>> d88900ff0a119a3c2aee9e6eb8ccfbe06b24cf23
