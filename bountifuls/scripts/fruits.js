0ca1cbe41ed5d4ec52b90b57e4653ab7348d8c7f
Peter Charles Otim <113760237+otimpeter@users.noreply.github.com>
2022-12-16 07:25:15 +0300

    updated
    added json

--- bountifuls/scripts/fruits.js
+++ bountifuls/scripts/fruits.js
@@ -1,5 +1,6 @@
 
-fetch('C:\Users\LENOVO\Desktop\week12\scripts\data.json')
+//('https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json')
+fetch('https://brotherblazzard.github.io/canvas-content/fruit.json')
 .then(res => {
     return res.json()
 }).then((data) => {
@@ -12,13 +13,13 @@ fetch('C:\Users\LENOVO\Desktop\week12\scripts\data.json')
     dataInfo.map((value) => {
 
         fruitsData += 
-        `<div class="friuts">
+        `<div class="card">
         <h3>${value.name} ${value.lastname}</h3>
         <p>Date of Birth ${value.birthdate}</p>
         <p>Place of Birth ${value.birthplace}</p>
         <img src="${value.imageurl}">
         </div>
-        `;
+     `;
     })
     document.querySelector('.items').innerHTML = fruitsData;
 
